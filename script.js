2b1ea6bbe720d84ac8ba0eabb0083749c7638db7
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-06-27 01:26:50 -0400

    updated vars on script and html location

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("generate");
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var punctuation = "~`!@#$%^&*()_+|}{:<>?.,;\][]=";
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() 
+var lowercaseInput= document.getElementById("lowercase");
+var uppercaseInput= document.getElementById("uppercase");
+var numbersInput= document.getElementById("numbers");
+var lowercaseInput= document.getElementById("lowercase");
+
+{
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var userpassword = "";
+  var passwordCharSet = "";
+
+  if (lowercaseInput.checked){
+    passwordCharSet+= lowercase;
+  }
+
+ if (uppercaseInput.checked){
+    passwordCharSet+= uppercase;
+  }
+  if (numbersInput.checked){
+    passwordCharSet+= numbers;
+  }
+  if (punctuationInput.checked){
+    passwordCharSet+= punctuation;
+  }
+  
+
+
+
+  var passwordText = document.querySelector("password");
 
   passwordText.value = password;
 
