852cbb80b2d8eadb2e6f8a4880d840447e3b1cb2
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-06-28 23:57:08 -0400

    added readme and changed color background

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# SecureMe
+#SecureMe
+
+## Description 
+
+Password Generator used for secure passwords that is selected with variable selections of numbers, 
+upper and lowercase letters and punctuations. This site allows users to select their entries and then
+generate a password. The final results allows them to copy and paste to different sources.
+
+## Installation
+
+Use of Bootstrap 4.0, Javascript, CSS, and DOM layout options. 
+
+## Usage 
+
+You have to click the "Generate Password" button and follow prompts concerning the choice selection of characters.
+Once you have completed the steps the new password will display onscreen and you can  then click "Copy Password" it and use it on different websites or security of various apps or home functions.
+
+
+## Credits
+ My classmates Ari, Chidinma, Sebastian and my TA Vince.
+
+## License
+opyright (c) 2020 Tia Smith
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Authors
+Tia Smith.
+
+## Acknowledgments
+My T.A. Vince Shury
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,82 @@
-
+// Button Generate and Copy Queries
 let generateButton = document.querySelector("#generate");
 let copyButton = document.querySelector("#copy");
 
+// Constant Like Variable Statements
 const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
 const numericalCharacters = "0123456789";
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
-
+// Function of  a New Generated Password
 function generatePassword() {
 
     let newPassword = "";
     let passwordCharset = "";
 
+// Prompt Length Question for Entry After Button Initated
     let passwordLengthInput = prompt("How many characters would you like your password to be?");
 
+// If and Return Conditionals for Choice Length of Input
     if (Number(passwordLengthInput < 8) | Number(passwordLengthInput > 128) | isNaN(passwordLengthInput)) {
-        alert("Please enter a number between 8 and 128!");
-        return;
+    alert("Please enter a number between 8 and 128!");
+    return;
     } 
-
     let lowercaseCharactersInput = confirm("Would you like lowercase letters to be included in your password?");
 
+// If and Then Conditionals for Lowercase Constant
     if (lowercaseCharactersInput) {
-        passwordCharset += lowercaseCharacters;
+    passwordCharset += lowercaseCharacters;
     }
-
     let uppercaseCharactersInput = confirm("Would you like uppercase letters to be included in your password?");
 
+// If and Then Conditionals for Uppercase Constant
     if (uppercaseCharactersInput) {
-        passwordCharset += uppercaseCharacters;
+    passwordCharset += uppercaseCharacters;
     }
-
     let numericalCharactersInput = confirm("Would you like numbers to be included in your password?");
 
+// If and Then Conditionals for Number Constant
     if (numericalCharactersInput) {
-        passwordCharset += numericalCharacters;
+    passwordCharset += numericalCharacters;
     }
-
     let specialCharactersInput = confirm("Would you like special characters to be included in your password?");
 
+// If and Then Conditionals for Punctuation Constant
     if (specialCharactersInput) {
-        passwordCharset += specialCharacters;
+    passwordCharset += specialCharacters;
     }
 
+// Conditional Character Selector of Choice
     if (passwordCharset === "") {
-        alert("Please select at least one type of character you would like to include!");
+    alert("Please select at least one type of character you would like to include!");
     }
-    
+
+// Conditional Choice Return from Selector 
     if (Number(passwordLengthInput >= 8) && Number(passwordLengthInput <= 128)) {
-        for (var i = 0; i < passwordLengthInput; i++) {
-            newPassword += passwordCharset.charAt(Math.floor(Math.random() * passwordCharset.length));
-        };
-        // return ("password", newPassword);
+    for (var i = 0; i < passwordLengthInput; i++) {
+    newPassword += passwordCharset.charAt(Math.floor(Math.random() * passwordCharset.length));
+    };
+
+// Return ("password", newPassword);
         return newPassword
     };
 };
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
-
 generateButton.addEventListener("click", writePassword);
 
-function copyPassword() {
+// Copy Function of New Password 
+    function copyPassword() {
     password.select();
     document.execCommand("copy");
     passwordTextarea.innerHTML = "";
+
 };
-copyButton.addEventListener("click", copyPassword);
+    copyButton.addEventListener("click", copyPassword);
